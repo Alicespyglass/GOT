99459cc7f003311c4b07e769663cd8a9309aaabb
Alice Cheung <alice@myurbanjungle.com>
2017-07-11 14:26:43 +0100

    hero-detail.component - inject ActiveRoute, HeroSerivce and Location services into constructor, saving values in private fields

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -2,8 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
 
-import { HeroService } from '.hero.service';
-import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'hero-detail',
@@ -21,4 +20,10 @@ import { Hero } from './hero';
 
 export class HeroDetailComponent {
   @Input() hero: Hero;
+
+  constructor (
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 }

4a8f5f6dd6a36c76e1a2076b9847a4bd92c6741c
Alice Cheung <alice@myurbanjungle.com>
2017-07-11 13:45:23 +0100

    get heroes. Dashboard.component - import Hero and Hero.service

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { Hero } from './hero';
+import { HeroService } from './hero.service'; 
+
 @Component({
   selector: 'my-dashboard',
   templateUrl: './dashboard.component.html'
